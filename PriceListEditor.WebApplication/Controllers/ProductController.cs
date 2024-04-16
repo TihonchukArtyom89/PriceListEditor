75f73aa68fa63b93853cf243b8cb52c601561f34
TihonchukArtyom89 <mortus1989@gmail.com>
2024-04-16 19:14:27 +0300

    add file _ViewImports cshtml to configure RazorEngine in Views folder, add file _ViewStart cshtml  in Views folder to set up for initial layout file named _Layout, add initial layout file (_Layour cshtml in Views/Shared folder), add files of controller (HomeController cs and ProductcController cs in Controller folder), add initial view files (Index cshtml in Views/Home folder and ProductList cshtml in Views/Product), add file of data model from previous version of project

---
+++ PriceListEditor.WebApplication/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceListEditor.WebApplication.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
